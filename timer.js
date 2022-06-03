fe3d70cdcc26ed03da05253b01e9b68443ae3813
Franco Castellacci <franco.castellacci@onapsis.com>
2022-06-03 11:23:32 -0300

    fixed interval delay

--- timer.js
+++ timer.js
@@ -1,16 +1,27 @@
 const daysElement = document.querySelector('.days');// span element that displays the amount of days
 
 const kennysLastAppearanceOnDynamite = new Date(2021, 11, 17, 11, 00, 00);
-daysSince(kennysLastAppearanceOnDynamite)
+countdownSince(kennysLastAppearanceOnDynamite)
 
-function daysSince(date){
+
+function daysSince(date) {
   // One day in milliseconds
   const dayInMs = 1000 * 60 * 60 * 24;
-  countdown = setInterval(()=>{
-		var now = Date.now(); 
-    var diff = now - date.getTime();
+  var now = Date.now(); 
+  var diff = now - date.getTime();
 
-    var daysSince = Math.round(diff / dayInMs);
-    daysElement.textContent = daysSince;
-	}, 1000); // every minute
+  var daysSince = Math.round(diff / dayInMs);
+  return daysSince;
 }
+
+function countdownSince(date) {
+  let days = daysSince(date);
+  daysElement.textContent = days; 
+  countdown = setInterval(() => {
+    let days = daysSince(date);
+    daysElement.textContent = days; 
+  }, 1000); //every second
+}
+
+
+
